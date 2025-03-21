70ae4fbec0dd9d49f673ca39b39c7e0c00dcce58
Murshed J. Ahmed <mahmed@marvel.com>
2025-03-21 10:50:18 -0400

    separate test files

---
+++ NodeTest.java
@@ -0,0 +1,46 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+
+    @org.junit.jupiter.api.Test
+    void testInvalidNodeThrowsException() {
+        try {
+            new Node(null, 10);
+            fail("An exception should be thrown when the Node value is null");
+        } catch (IllegalArgumentException e) {
+            // Do nothing
+        }
+
+        try {
+            new Node("Cat", -10);
+            fail("An exception should be thrown when the Node weight is negative");
+        } catch (IllegalArgumentException e) {
+            // Do nothing
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void testNodeDefaultConstructor() {
+        Node n = new Node();
+        assertEquals("", n.getTerm().getTerm());
+        assertEquals(0, n.getTerm().getWeight());
+        assertEquals(26, n.getReferences().length);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testNodeConstructor() {
+        Node n = new Node("Tiger", 20);
+        assertEquals("Tiger", n.getTerm().getTerm());
+        assertEquals(20, n.getTerm().getWeight());
+        assertEquals(26, n.getReferences().length);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testNodeSetTerm() {
+        Node n = new Node("Tiger", 20);
+        n.setTerm(new Term("Donkey", 99));
+        assertEquals("Donkey", n.getTerm().getTerm());
+        assertEquals(99, n.getTerm().getWeight());
+        assertEquals(26, n.getReferences().length);
+    }
+}
